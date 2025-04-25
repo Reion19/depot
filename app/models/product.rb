c3e9ba222ba1f7f9972fc1d56f4ebec5a8ab8eb5
Reion19 <reizu1906@gmail.com>
2025-04-25 18:45:40 +0300

    iteration b2 | tests -> add

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: { message: 'the title must be unique for all products!' }
+  validates :title, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with: %r{/\.(gif|jpg|png)\z/}i,
+    with: /\.(gif|jpg|png)\z/i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 end
