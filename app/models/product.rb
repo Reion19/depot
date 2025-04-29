cdf940a390fcddf94818be858d033c21ca82b03a
Reion19 <reizu1906@gmail.com>
2025-04-29 12:00:50 +0300

    iteration D2 | line items scaffold -> generated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :title, length: { minimum: 10 }
@@ -9,4 +13,11 @@ class Product < ApplicationRecord
     with: /\.(gif|jpg|png)\z/i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  def ensure_not_referenced_by_any_line_items
+    return if line_items.empty?
+
+    errors.add(:base, 'Line Items present')
+    throw :abort
+  end
 end
