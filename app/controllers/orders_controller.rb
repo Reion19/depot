487cfd29f505596ab6ab0895b37a941024495a63
Reion19 <reizu1906@gmail.com>
2025-05-10 17:53:12 +0300

    iteration H1 | mail send functional -> add

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,6 +30,8 @@ class OrdersController < ApplicationController
     respond_to do |format|
       if @order.save
         Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        OrderMailer.received(@order).deliver_later
         format.html { redirect_to store_index_path, notice: 'Tank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
