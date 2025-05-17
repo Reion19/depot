7907696b4e5aad756d0fd1712210a227ce5ffec2
Reion19 <reizu1906@gmail.com>
2025-05-17 13:14:42 +0300

    iteration I3 | authorize method -> add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :authorize
+
+  protected
+
+  def authorize
+    return if User.find_by(id: session[:user_id])
+
+    redirect_to login_url, notice: 'Please log in'
+  end
 end
